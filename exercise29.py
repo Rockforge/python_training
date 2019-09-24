f45413da336c6f0cd865db4280b51edf3bbfbbe5
Jurilla <Christian.Jurilla@ingrammicro.com>
2019-09-24 16:44:26 +0800

    Changed an example

--- exercise29.py
+++ exercise29.py
@@ -6,6 +6,12 @@ def greet(name, age = 16):
     print('Hello ' + name)
     print('You are ' + str(age))
 
+def getName():
+    return 'Christian'
+
+def getAge():
+    return 20
+
 # Positional arguments
 greet('Christian Jurilla')
 greet('Christian Jurilla', 20)
@@ -13,11 +19,10 @@ greet('Christian Jurilla', 20)
 # Named arguments / Keyword arguments
 greet(age=25, name='Christian',)
 
-
-params = {
-    'name': 'Sony Valdez',
-    'age': 20
-}
+params = {}
+params['name'] = getName()
+params['age'] = getAge()
 
 # Access the dict by values
 greet(**params)
+
