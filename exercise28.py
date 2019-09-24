5a1199d7b3255038f46abdd5a1b18f323b43e79c
Jurilla <Christian.Jurilla@ingrammicro.com>
2019-09-24 16:29:01 +0800

    Added random library`

---
+++ exercise28.py
@@ -0,0 +1,22 @@
+# exercise28.py
+# Random
+
+import random # Using this library
+
+heroes = ['Batman', 'Kardo Dalisay', 'Thanos', 'Thor', 'Saitama']
+
+best = random.choice(heroes)
+power = random.randint(9000, 10000)
+
+template = 'The best hero is {}. Their power level is {}'
+output = template.format(best, power)
+
+print(output)
+
+if power > 9000:
+    print('Their power level is over 9000!!!')
+
+
+
+
+
